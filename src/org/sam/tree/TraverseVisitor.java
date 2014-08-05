df264d0f3cbdfeb5ca2fd873284379c11589adc3
samuelsmal <samuel.vonbaussnern@gmail.com>
2014-08-05 15:29:30 +0200

    Add crossover test

--- src/org/sam/tree/TraverseVisitor.java
+++ src/org/sam/tree/TraverseVisitor.java
@@ -1,18 +1,34 @@
 package org.sam.tree;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by samuel on 05/08/14.
  */
 public class TraverseVisitor implements INodeVisitor {
     private ArrayList<INode> nodes;
+    private static final Random rand = new Random();
 
     public TraverseVisitor() {
         nodes = new ArrayList<INode>();
     }
 
     public TraverseVisitor(ArrayList<INode> nodes) {
+        this.nodes = nodes;
+    }
+
+    public INode getRandomNode() {
+        // 0 (inclusive) and the specified value (exclusive)
+        return nodes.get(rand.nextInt(nodes.size()));
+    }
+
+    public List<INode> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(ArrayList<INode> nodes) {
         this.nodes = nodes;
     }
 
---
+++ src/test/test/tree/CrossoverTest.java
@@ -0,0 +1,54 @@
+package test.tree;
+
+import org.junit.Test;
+import org.sam.tree.FullTree;
+import org.sam.tree.INode;
+import org.sam.tree.TraverseVisitor;
+import org.sam.tree.Tree;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by samuel on 05/08/14.
+ */
+public class CrossoverTest {
+
+    @Test
+    public void coTest() {
+        Tree a = new FullTree();
+        Tree b = new FullTree();
+
+        TraverseVisitor crossOverVisitorA = new TraverseVisitor();
+        TraverseVisitor crossOverVisitorB = new TraverseVisitor();
+
+        a.getRoot().visit(crossOverVisitorA);
+        b.getRoot().visit(crossOverVisitorB);
+
+        INode aNode = crossOverVisitorA.getRandomNode();
+        INode aaNode = aNode;
+        INode bNode = crossOverVisitorB.getRandomNode();
+        INode bbNode = bNode;
+        INode tmpNode = bNode;
+
+        bNode = aNode;
+        aNode = tmpNode;
+
+        assertFalse(aNode == aaNode);
+        assertTrue(bNode == aaNode);
+        assertFalse(bNode == bbNode);
+        assertTrue(aNode == bbNode);
+
+        crossOverVisitorA.setNodes(new ArrayList<INode>());
+        crossOverVisitorB.setNodes(new ArrayList<INode>());
+
+        a.getRoot().visit(crossOverVisitorA);
+        b.getRoot().visit(crossOverVisitorB);
+
+        assertFalse(crossOverVisitorA.getNodes().contains(aNode));
+        assertTrue(crossOverVisitorA.getNodes().contains(bNode));
+        assertFalse(crossOverVisitorB.getNodes().contains(bNode));
+        assertTrue(crossOverVisitorB.getNodes().contains(aNode));
+    }
+}
