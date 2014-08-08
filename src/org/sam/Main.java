a3a93fbdcb0e823150da6f0ba6aa1ea6deee02ca
samuelsmal <samuel.vonbaussnern@gmail.com>
2014-08-08 10:53:58 +0200

    random tests in main

--- src/org/sam/Main.java
+++ src/org/sam/Main.java
@@ -1,9 +1,6 @@
 package org.sam;
 
-import org.sam.game.GPTreePlayer;
-import org.sam.game.Game;
-import org.sam.game.HumanPlayer;
-import org.sam.game.RandomPlayer;
+import org.sam.game.*;
 import org.sam.genetics.Evolution;
 import org.sam.tree.*;
 import org.sam.tree.BinaryFunctions.AddOperationNode;
@@ -28,5 +25,19 @@ public class Main {
 
         System.out.println("FirstPlayerColour: " + Game.FIRST_PLAYER_COLOUR);
         System.out.println("Player " + game.colourOfWinner() + " has won!\n" + game);
+
+        //System.out.println(gpTreePlayer);
+
+        Game game2 = new Game();
+        game2.startGame(new RandomPlayer(), new GPPlayerRun0());
+        System.out.println("Player " + game2.colourOfWinner() + " has won!\n" + game);
+
+        Game game3 = new Game();
+        game3.startGame(new RandomPlayer(), new GPTreePlayer(TreeFactory.fullTree(10)));
+        System.out.println("Player " + game3.colourOfWinner() + " has won!\n" + game);
+
+        Game gama4 = new Game();
+        gama4.startGame(new GPTreePlayer(TreeFactory.fullTree(2)), new GPTreePlayer(TreeFactory.fullTree(12)));
+        System.out.println("Player " + gama4.colourOfWinner() + " has won!\n" + game);
     }
 }
